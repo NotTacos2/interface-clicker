b83d726933cd9766192b921d5dbaede9b1d346f9
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2025-12-19 09:01:01 -0700

    add working robot

--- control.gd
+++ control.gd
@@ -3,7 +3,6 @@ var config = ConfigFile.new()
 var amount = 0
 var item = 0
 var robot = 0
-var time = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,6 +15,8 @@ func _ready() -> void:
 		amount = config.get_value("player", "score")
 		item = config.get_value("player", "golditem")
 		robot = config.get_value("player", "robot")
+	
+	$Timer.timeout.connect(_addpointforrobot)
 		
 	if amount >= 100:
 		$Label3.text = "ooo 100"
@@ -35,14 +36,24 @@ func _buttonpressed():
 	$Label2.text = "Clicked: " + str(amount);
 	config.set_value("player", "score", amount)
 	config.save("user://clicker.cfg")
-	if amount == 100:
+	if amount >= 100:
 		$Label3.text = "ooo 100"
-	if amount == 200:
+	if amount >= 200:
 		$Label3.text = "200 nice"
-	if amount == 300:
+	if amount >= 300:
 		$Label3.text = "wow 300"
 func _button2pressed():
 	get_tree().change_scene_to_file("res://shop.tscn")
 	
 func _settingsbuttonpressed():
 	get_tree().change_scene_to_file("res://settings.tscn")
+	
+func _addpointforrobot():
+	var save = config.load("user://clicker.cfg")
+	if save == OK:
+		robot = config.get_value("player", "robot")
+	if robot == 1:
+		amount += 1
+		config.set_value("player", "score", amount)
+		config.save("user://clicker.cfg")
+		$Label2.text = "Clicked: " + str(amount);
